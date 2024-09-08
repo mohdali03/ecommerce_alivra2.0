fab298f9e0ac4d89a52199a8100b0fd4efa32ee9
mohdali03 <shaikhali6507@gmail.com>
2024-09-08 12:40:21 +0530

    final commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.alvirabags.in', // Your website URL
+    generateRobotsTxt: true, // Optional: Generate a robots.txt file
+    sitemapSize: 5000, // Limit entries per sitemap (default is 5000)
+  }
